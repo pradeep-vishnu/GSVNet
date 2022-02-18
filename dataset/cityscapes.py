cdaf9b5ec6d812dc43aa006167da5ee3bbb94e31
Vishnu Pradeep <61411495+pradeep-vishnu@users.noreply.github.com>
2022-02-18 13:33:31 +0100

    datapath update

--- dataset/cityscapes.py
+++ dataset/cityscapes.py
@@ -28,11 +28,11 @@ class cityscape_dataset(Dataset):
         self.class_map = dict(zip(self.valid_classes, range(len(self.valid_classes))))
 
         if mode == 'train' :
-            file_path = "./dataset/cityscapes/train.txt"
+            file_path = "~/repos/GSVNet/dataset/cityscapes/train.txt"
         elif mode == 'val':
-            file_path = "./dataset/cityscapes/val.txt"
+            file_path = "~/repos/GSVNet/dataset/cityscapes/val.txt"
         elif mode == 'test':
-            file_path = "./dataset/cityscapes/test.txt"
+            file_path = "~/repos/GSVNet/dataset/cityscapes/test.txt"
 
         self.file_list = []
         
