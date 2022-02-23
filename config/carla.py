0da3c45ef34ce2f2e3af2119d95ed02926e5b06b
pradeep-vishnu <mevishnupradeep@gmail.com>
2022-02-23 11:40:24 +0100

    datapath update

--- config/carla.py
+++ config/carla.py
@@ -28,9 +28,9 @@ class carla_config(object):
         ('void', (0, 0, 0), ignore_index)
     ] 
 
-    swnet_weight_path = './weights/cityscapes-swnet-R18.pt'
-    bsnet_weight_path = './weights/cityscapes-bisenet-R18.pth'
-    bisenet_resume_path = './weights/gsvnet_bisenet_r18.tar'
-    swnet_resume_path = './weights/gsvnet_swnet_r18.tar'
-    optical_flow_network_path = './weights/flownet.pth.tar'
-    data_path = 'gpfs1/dlocal/home/2020010/PARTAGE/vp/carla' # put your data path here
+    swnet_weight_path = '/gpfs1/home/2018015/mgrass01/repos/GSVNet/weights/cityscapes-swnet-R18.pt'
+    bsnet_weight_path = '/gpfs1/home/2018015/mgrass01/repos/GSVNet/weights/cityscapes-bisenet-R18.pth'
+    bisenet_resume_path = '/gpfs1/home/2018015/mgrass01/repos/GSVNet/weights/gsvnet_bisenet_r18.tar'
+    swnet_resume_path = '/gpfs1/home/2018015/mgrass01/repos/GSVNet/weights/gsvnet_swnet_r18.tar'
+    optical_flow_network_path = '/gpfs1/home/2018015/mgrass01/repos/GSVNet/weights/flownet.pth.tar'
+    data_path = '/gpfs1/dlocal/home/2018015/PARTAGE/dataset/carla' # put your data path here
